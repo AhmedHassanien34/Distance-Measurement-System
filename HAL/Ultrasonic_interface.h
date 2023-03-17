c58ec2fbe37784a1b102a7119d1af260a3170fe4
Ahmed Torres <AhmedTorres2000@gmail.com>
2023-03-17 12:28:18 +0200

    Distance Measurement System

---
+++ HAL/Ultrasonic_interface.h
@@ -0,0 +1,20 @@
+/**************************************************************************************/
+/********************			Author: Ahmed Mohamed			***********************/
+/********************			Layer: HAL			 			***********************/
+/********************			Component: Ultrasonic 		    ***********************/
+/********************			Version: 1.00		 			***********************/
+/********************			Date: 16-12-2022	 			***********************/
+/**************************************************************************************/
+
+#ifndef ULTRASONIC_INTERFACE_H_
+#define ULTRASONIC_INTERFACE_H_
+
+void ULTRASONIC_voidEdgeProcessing(void);
+
+void ULTRASONIC_voidInit(void);
+
+void ULTRASONIC_voidTrigger(void);
+
+u16 ULTRASONIC_u16GetDistance(void);
+
+#endif
